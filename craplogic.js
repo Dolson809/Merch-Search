b9a3da2285042d839383d33aa847996fd0dc8141
Magee100 <magee100@gmail.com>
2019-03-28 18:57:44 -0700

    basic image array created, col/rows added

---
+++ craplogic.js
@@ -0,0 +1,30 @@
+var Deflect = [
+    'Images/Deflect/DemonLogo-MD-Front.jpg',
+    'Images/Deflect/DemonLogo-MD-Back.jpg',
+    'Images/Deflect/DemonLogo-HOA-TC-Front.jpg',
+    'Images/Deflect/DemonLogo-HOA-TC-Back.jpg',
+];
+var Illusion = [
+    'Images/Illusion/HandLogo-Grey.jpg',
+    'Images/Illusion/HandLogo-Hoodie.jpg',
+    'Images/Illusion/HandLogo-Yellow.jpg',
+    'Images/Illusion/Cards-MWAS.jpg',
+];
+var IronAge = [
+    'Images/Iron-Age/Champion.jpg',
+    'Images/Iron-Age/EvilWays.jpg',
+    'Images/Iron-Age/Savage.jpg',
+    'Images/Iron-Age/TwistedFaith.jpg',
+];
+var Lock = [
+    'Images/Lock/NTN-Tour-Front.jpg',
+    'Images/Lock/NTN-Tour-Back.jpg',
+    'Images/Lock/Texas-Tour-Front.jpg',
+    'Images/Lock/Texas-Tour-Back.jpg',
+];
+var SmashingPumpkins = [
+    'Images/Smashing-Pumpkins/InfiniteSadness-Front.jpg',
+    'Images/Smashing-Pumpkins/InfiniteSadness-Back.jpg',
+    'Images/Smashing-Pumpkins/Circus.jpg',
+    'Images/Smashing-Pumpkins/Disco.jpg',
+];
