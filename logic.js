d430bbd7167acebd7decf6d3a7d98bf0297b0a0e
jason <jasonkellogglee@gmail.com>
2019-04-01 11:54:11 -0700

    added javascript link to html

--- logic.js
+++ logic.js
@@ -6,28 +6,27 @@ $('#search-form').on('submit', function (event) {
     var search = $("#search").val();
     function showPosition(position) {
         var x = document.getElementById("location");
-        x.innerHTML = "Latitude: " + position.coords.latitude + 
-        "<br>Longitude: " + position.coords.longitude; 
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
         var latlon = position.coords.latitude + "," + position.coords.longitude;
-      
-      
+
         $.ajax({
-          type:"GET",
-          url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&keyword=" + search + "&latlong="+latlon,
-          async:true,
-          dataType: "json",
-          success: function(json) {
+            type: "GET",
+            url: "https://app.ticketmaster.com/discovery/v2/events.json?apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&keyword=" + search + "&latlong=" + latlon,
+            async: true,
+            dataType: "json",
+            success: function (json) {
                 console.log(json);
                 var e = document.getElementById("events");
                 e.innerHTML = json.page.totalElements + " events found.";
                 showEvents(json);
                 initMap(position, json);
             },
-          error: function(xhr, status, err) {
+            error: function (xhr, status, err) {
                 console.log(err);
             }
         });
-      
+
     }
     function getLocation() {
         if (navigator.geolocation) {
@@ -37,10 +36,9 @@ $('#search-form').on('submit', function (event) {
             x.innerHTML = "Geolocation is not supported by this browser.";
         }
     }
-      
-      
-      function showError(error) {
-        switch(error.code) {
+
+    function showError(error) {
+        switch (error.code) {
             case error.PERMISSION_DENIED:
                 x.innerHTML = "User denied the request for Geolocation."
                 break;
@@ -55,6 +53,31 @@ $('#search-form').on('submit', function (event) {
                 break;
         }
     }
+    function showEvents(json) {
+        for (var i = 0; i < json.page.size; i++) {
+            $("#events").append("<p>" + json._embedded.events[i].name + "</p>");
+        }
+    }
+
+    function initMap(position, json) {
+        var mapDiv = document.getElementById('map');
+        map = new google.maps.Map(mapDiv, {
+            center: { lat: position.coords.latitude, lng: position.coords.longitude },
+            zoom: 10
+        });
+        for (var i = 0; i < json.page.size; i++) {
+            addMarker(map, json._embedded.events[i]);
+        }
+    }
+
+    function addMarker(map, event) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
+            map: map
+        });
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+        console.log(marker);
+    }
     getLocation();
 })
 
@@ -62,32 +85,7 @@ $('#search-form').on('submit', function (event) {
 
 
 
-function showEvents(json) {
-for(var i=0; i<json.page.size; i++) {
-  $("#events").append("<p>"+json._embedded.events[i].name+"</p>");
-}
-}
 
-
-function initMap(position, json) {
-var mapDiv = document.getElementById('map');
- map = new google.maps.Map(mapDiv, {
-  center: {lat: position.coords.latitude, lng: position.coords.longitude},
-  zoom: 10
-});
-for(var i=0; i<json.page.size; i++) {
-  addMarker(map, json._embedded.events[i]);
-}
-}
-
-function addMarker(map, event) {
-var marker = new google.maps.Marker({
-  position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
-  map: map
-});
-marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
-console.log(marker);
-}
 
 
 
@@ -120,7 +118,7 @@ console.log(marker);
 
 // // Whenever a user clicks the submit button
 // $("#submit").on("click", function(event) {
-    
+
 //     // Prevent form from submitting with event.preventDefault() or returning false
 //     event.preventDefault();
 
