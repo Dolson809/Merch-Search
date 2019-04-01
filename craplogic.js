0530c11548f075f2bb14e3e826171c1f73211dae
Magee100 <magee100@gmail.com>
2019-04-01 11:01:58 -0700

    changed array info, slightly updated css

--- craplogic.js
+++ craplogic.js
@@ -1,27 +1,36 @@
-var Deflect = [
-    'Images/Deflect/DemonLogo-MD-Front.jpg',
-    'Images/Deflect/DemonLogo-MD-Back.jpg',
-    'Images/Deflect/DemonLogo-HOA-TC-Front.jpg',
-    'Images/Deflect/DemonLogo-HOA-TC-Back.jpg',
+// var Deflect = [
+//     'Images/Deflect/DemonLogo-MD-Front.jpg',
+//     'Images/Deflect/DemonLogo-MD-Back.jpg',
+//     'Images/Deflect/DemonLogo-HOA-TC-Front.jpg',
+//     'Images/Deflect/DemonLogo-HOA-TC-Back.jpg',
+// ];
+// var Illusion = [
+//     'Images/Illusion/HandLogo-Grey.jpg',
+//     'Images/Illusion/HandLogo-Hoodie.jpg',
+//     'Images/Illusion/HandLogo-Yellow.jpg',
+//     'Images/Illusion/Cards-MWAS.jpg',
+// ];
+// var IronAge = [
+//     'Images/Iron-Age/Champion.jpg',
+//     'Images/Iron-Age/EvilWays.jpg',
+//     'Images/Iron-Age/Savage.jpg',
+//     'Images/Iron-Age/TwistedFaith.jpg',
+// ];
+
+var MacDemarco = [
+    'Images/Mac-Demarco/Futbol.png',
+    'Images/Mac-Demarco/Ponies.jpg',
+    'Images/Mac-Demarco/ThisOldDog.jpg',
+    'Images/Mac-Demarco/ThisOldDogArt.jpg',
 ];
-var Illusion = [
-    'Images/Illusion/HandLogo-Grey.jpg',
-    'Images/Illusion/HandLogo-Hoodie.jpg',
-    'Images/Illusion/HandLogo-Yellow.jpg',
-    'Images/Illusion/Cards-MWAS.jpg',
+
+var TameImpala = [
+    'Images/Tame-Impala/Burgundy.jpg',
+    'Images/Tame-Impala/Longsleeve.jpg',
+    'Images/Tame-Impala/Pitchfork.jpg',
+    'Images/Tame-Impala/RedBlue.jpg',
 ];
-var IronAge = [
-    'Images/Iron-Age/Champion.jpg',
-    'Images/Iron-Age/EvilWays.jpg',
-    'Images/Iron-Age/Savage.jpg',
-    'Images/Iron-Age/TwistedFaith.jpg',
-];
-var Lock = [
-    'Images/Lock/NTN-Tour-Front.jpg',
-    'Images/Lock/NTN-Tour-Back.jpg',
-    'Images/Lock/Texas-Tour-Front.jpg',
-    'Images/Lock/Texas-Tour-Back.jpg',
-];
+
 var SmashingPumpkins = [
     'Images/Smashing-Pumpkins/InfiniteSadness-Front.jpg',
     'Images/Smashing-Pumpkins/InfiniteSadness-Back.jpg',
