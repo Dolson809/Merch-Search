5546673728b542a98a9d10b0381aabb69465a7ab
Andrew Magee <magee100@gmail.com>
2019-04-02 13:23:25 -0700

    Stole some js from john

--- craplogic.js
+++ craplogic.js
@@ -1,31 +1,95 @@
-var MacDemarco = [
-    'Images/Mac-Demarco/Futbol.png',
-    'Images/Mac-Demarco/Ponies.jpg',
-    'Images/Mac-Demarco/ThisOldDog.jpg',
-    'Images/Mac-Demarco/ThisOldDogArt.jpg',
-];
+$(document).ready(function(){
+  
 
-var TameImpala = [
-    'Images/Tame-Impala/Burgundy.jpg',
-    'Images/Tame-Impala/Longsleeve.jpg',
-    'Images/Tame-Impala/Pitchfork.jpg',
-    'Images/Tame-Impala/RedBlue.jpg',
-];
-
-var SmashingPumpkins = [
-    'Images/Smashing-Pumpkins/InfiniteSadness-Front.jpg',
-    'Images/Smashing-Pumpkins/InfiniteSadness-Back.jpg',
-    'Images/Smashing-Pumpkins/Circus.jpg',
-    'Images/Smashing-Pumpkins/Disco.jpg',
-];
-
-$("#artist").on('submit', function (event) {
-    event.preventDefault();
-    var search = $("#artist").val();
-    alert("DINGUS");
-    console.log(search);
-    console.log("CMON");
-});
-
-var artistInput = document.getElementById('artist');
+    $('#search-form').on('submit', function (event) {
+        event.preventDefault();
+        var search = $("#search").val();
+        function showPosition(position) {
+            var x = document.getElementById("location");
+            x.innerHTML = "Latitude: " + position.coords.latitude + 
+            "<br>Longitude: " + position.coords.longitude; 
+            var latlon = position.coords.latitude + "," + position.coords.longitude;
+          
+          
+            $.ajax({
+              type:"GET",
+              url:"https://app.ticketmaster.com/discovery/v2/events.json?apikey=5QGCEXAsJowiCI4n1uAwMlCGAcSNAEmG&keyword=" + search + "&latlong="+latlon,
+              async:true,
+              dataType: "json",
+              success: function(json) {
+                    console.log(json);
+                    var e = document.getElementById("events");
+                    e.innerHTML = json.page.totalElements + " events found.";
+                    showEvents(json);
+                    initMap(position, json);
+                },
+              error: function(xhr, status, err) {
+                    console.log(err);
+                }
+            });
+          
+        }
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition, showError);
+            } else {
+                var x = document.getElementById("location");
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+          
+          
+          function showError(error) {
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    x.innerHTML = "User denied the request for Geolocation."
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    x.innerHTML = "Location information is unavailable."
+                    break;
+                case error.TIMEOUT:
+                    x.innerHTML = "The request to get user location timed out."
+                    break;
+                case error.UNKNOWN_ERROR:
+                    x.innerHTML = "An unknown error occurred."
+                    break;
+            }
+        }
+        getLocation();
+    })
+    
+    
+    
+    
+    
+    function showEvents(json) {
+    for(var i=0; i<json.page.size; i++) {
+      $("#events").append("<p>"+json._embedded.events[i].name+"</p>");
+    }
+    }
+    
+    
+    function initMap(position, json) {
+      var map;
+      console.log(map);
+      var mapDiv = document.getElementById('map-display');
+      map = new google.maps.Map(mapDiv, {
+      center: {lat: position.coords.latitude, lng: position.coords.longitude},
+      zoom: 12
+    });
+    for(var i=0; i<json.page.size; i++) {
+      addMarker(map, json._embedded.events[i]);
+    }
+    }
+    
+    function addMarker(map, event) {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(event._embedded.venues[0].location.latitude, event._embedded.venues[0].location.longitude),
+      map: map
+    });
+    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    console.log(marker);
+    }
+    })
+    
 
