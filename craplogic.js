1b19168e75f6779a31e24eb1709b2c39d91f64b1
Magee100 <magee100@gmail.com>
2019-04-01 20:48:43 -0700

    event info container/classes created

--- craplogic.js
+++ craplogic.js
@@ -1,22 +1,3 @@
-// var Deflect = [
-//     'Images/Deflect/DemonLogo-MD-Front.jpg',
-//     'Images/Deflect/DemonLogo-MD-Back.jpg',
-//     'Images/Deflect/DemonLogo-HOA-TC-Front.jpg',
-//     'Images/Deflect/DemonLogo-HOA-TC-Back.jpg',
-// ];
-// var Illusion = [
-//     'Images/Illusion/HandLogo-Grey.jpg',
-//     'Images/Illusion/HandLogo-Hoodie.jpg',
-//     'Images/Illusion/HandLogo-Yellow.jpg',
-//     'Images/Illusion/Cards-MWAS.jpg',
-// ];
-// var IronAge = [
-//     'Images/Iron-Age/Champion.jpg',
-//     'Images/Iron-Age/EvilWays.jpg',
-//     'Images/Iron-Age/Savage.jpg',
-//     'Images/Iron-Age/TwistedFaith.jpg',
-// ];
-
 var MacDemarco = [
     'Images/Mac-Demarco/Futbol.png',
     'Images/Mac-Demarco/Ponies.jpg',
@@ -37,3 +18,14 @@ var SmashingPumpkins = [
     'Images/Smashing-Pumpkins/Circus.jpg',
     'Images/Smashing-Pumpkins/Disco.jpg',
 ];
+
+$("#artist").on('submit', function (event) {
+    event.preventDefault();
+    var search = $("#artist").val();
+    alert("DINGUS");
+    console.log(search);
+    console.log("CMON");
+});
+
+var artistInput = document.getElementById('artist');
+
